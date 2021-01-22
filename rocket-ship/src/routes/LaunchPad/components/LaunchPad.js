1dcbd7219f8d6e9df23b9a00ebd9438c61237cbd
Shams alam naimi <60454351+shamsAlamNaimi@users.noreply.github.com>
2021-01-22 13:25:50 -0500

    Update LaunchPad.js
    solution:1 after every 500ms the state rerenderCount is being updated thus causing the function to re-render
      in order to stop this I just commented this function of

--- rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
+++ rocket-ship/src/routes/LaunchPad/components/LaunchPad.js
@@ -5,7 +5,11 @@ import '../styles/_launchpad.scss';
 export default function LaunchPad() {
   const [rerenderCount, triggerRerender] = useState(0);
 
-  setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+   //after every 500ms the state rerenderCount is being updated thus causing the function to re-render
+  //in order to stop this i just commented this function of
+  ///////////////changes////////////////////////
+  //setTimeout(() => { triggerRerender(rerenderCount + 1); }, 500);
+  /////////////changes////////////////////////
 
   return (
     <div className="launchpad">
